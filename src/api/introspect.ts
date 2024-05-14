94ed1cef268e2f21eeb79548c73f37d935a4caf9
Jacob Weinbren <jacobweinbren@gmail.com>
2024-05-14 12:53:00 +0100

    Adds introspect request

---
+++ src/api/introspect.ts
@@ -0,0 +1,28 @@
+import { APIRoute } from "astro";
+import { introspectAccessToken } from "../urls.js";
+import config from "virtual:kinde-integration/config";
+
+export const GET: APIRoute = async ({ request }) => {
+    const cookies = request.headers.get("cookie");
+    const accessToken = cookies
+        ?.split("; ")
+        .find((c) => c.startsWith("kinde_access_token="))
+        ?.split("=")[1];
+
+    if (!accessToken) {
+        return new Response("No access token found", { status: 401 });
+    }
+
+    try {
+        const introspectionResult = await introspectAccessToken(
+            accessToken,
+            config
+        );
+        return new Response(JSON.stringify(introspectionResult), {
+            status: 200,
+            headers: { "Content-Type": "application/json" },
+        });
+    } catch (error) {
+        return new Response(error.message, { status: 500 });
+    }
+};
